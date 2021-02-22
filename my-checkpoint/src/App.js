f1ecac544780ebbe73710d9b1ae23c7d8b9fc3b3
ziede <ziede.ben.yahya@gmail.com>
2021-02-22 23:17:48 +0100

    bootstrap from added

--- my-checkpoint/src/App.js
+++ my-checkpoint/src/App.js
@@ -1,41 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import { FormControl, Row } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container'
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Col, FormControl, Row } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div >
+    <div>
       <Container>
-        
-      <Form>
-    <Row>
-     <col></col> 
-    <Form.Group controlId="formBasicEmail">
-      <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-      <Form.Text className="text-muted">
-        We'll never share your email with anyone else.hello
-      </Form.Text>
-    </Form.Group>
-  
-    <Form.Group controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    </Row>
-    
-    <Form.Group controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" /> 
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
-      </Container>
+        <Form>
+          <Row>
+            <Col>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.hello
+              </Form.Text>
+            </Form.Group>
+            </Col>
+            
+            <Col >
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            </Col>
+          </Row>
 
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 }
